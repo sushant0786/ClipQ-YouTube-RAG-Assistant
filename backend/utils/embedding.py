a719039462ca78a81b2381cdd5012e17aed4dae0
sushant0786 <sushant.bodade22@spit.ac.in>
2025-07-30 20:19:02 +0530

    Update embedding.py

--- backend/utils/embedding.py
+++ backend/utils/embedding.py
@@ -3,6 +3,7 @@ from backend.utils.genai_clent import get_client
 from google.genai import types
 from chromadb import EmbeddingFunction, Documents, Embeddings
 
+
 EMBEDDING_MODEL_ID = "models/embedding-001"
 
 class GeminiEmbeddingFunction(EmbeddingFunction):
